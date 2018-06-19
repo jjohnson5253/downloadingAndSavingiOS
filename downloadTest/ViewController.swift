bd878f4df9796a38404a50855a10f56d3b7243dd
Jake Johnson <jakejohnson@jakes-mbp.attlocal.net>
2018-06-18 23:53:52 -0500

    works

--- downloadTest/ViewController.swift
+++ downloadTest/ViewController.swift
@@ -7,61 +7,42 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController, URLSessionDownloadDelegate {
+class ViewController: UIViewController, WKUIDelegate {
     
-    // Create a FileManager instance
-    let fileManager = FileManager.default
-    
-    let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    var webView: WKWebView!
 
-    var downloadSession = URLSession()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let url = URL(string: "https://www.apple.com")!
         
-        
-        let url = URL(string: "https://phet.colorado.edu/sims/html/circuit-construction-kit-dc-virtual-lab/latest/circuit-construction-kit-dc-virtual-lab_en.html?download")!
-        
-        
-        let task = downloadSession.shared.downloadTask(with: url) { localURL, urlResponse, error in}
-    
+        let task = URLSession.shared.downloadTask(with: url) { localURL, urlResponse, error in
+            if let localURL = localURL {
+                if let html = try? String(contentsOf: localURL) {
+                    UserDefaults.standard.set(html, forKey: "circuits")
+                }
+            }
+        }
         
         task.resume()
         
-    }
-    
-    func localFilePath(for url: URL) -> URL {
-        return documentsPath.appendingPathComponent(url.lastPathComponent)
-    }
-
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        // Setting
         
-        guard let sourceURL = downloadTask.originalRequest?.url else { return }
+        let defaults = UserDefaults.standard
+        defaults.set("Some String Value", forKey: "circuits")
 
-        // 2
-        let destinationURL = localFilePath(for: sourceURL)
-        print(destinationURL)
-        // 3
-        try? fileManager.removeItem(at: destinationURL)
-        do {
-            try fileManager.copyItem(at: location, to: destinationURL)
-//            download?.track.downloaded = true
-        } catch let error {
-            print("Could not copy file to disk: \(error.localizedDescription)")
-        }
-        // 4
-//        if let index = download?.track.index {
-//            DispatchQueue.main.async {
-//                self.tableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .none)
-//            }
+        // Getting
+        
+//        let defaults = UserDefaults.standard
+//        if let stringOne = defaults.string(forKey: "circuits") {
+//            print(stringOne) // Some String Value
 //        }
-        
-    }
-    
-    
 
+    
+    }
 
 }
 
